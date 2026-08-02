76d81680e9ca972de918e2c5e8870084b29a2386
BlueberryDari <avlertaver@gmail.com>
2026-08-02 19:27:44 +0300

    solo task is edited

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -5,81 +5,46 @@ class Game {
     this.winsElement = container.querySelector('.status__wins');
     this.lossElement = container.querySelector('.status__loss');
 
-    this.reset();
+    this.intervalId = null;   
+    this.timeLimit = 0; 
 
+    this.reset();
     this.registerEvents();
   }
 
   reset() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+
     this.setNewWord();
     this.winsElement.textContent = 0;
     this.lossElement.textContent = 0;
   }
 
-  let intervalId = null;   
-  let timeLimit = 0; 
+ 
 
   registerEvents() {
-    
-    
+        
     document.addEventListener("keyup", (evt) => {
       if (evt.key.length > 1) return;
-const necessarySymbol = this.currentSymbol.textContent.toLowerCase();
-
-    const typedSymbol = evt.key.toLowerCase();
+      const necessarySymbol = this.currentSymbol.textContent.toLowerCase();
+      const typedSymbol = evt.key.toLowerCase();
 
     if (necessarySymbol === typedSymbol) {
      this.success();
     } else {
       this.fail();
     }
-    })
-    
-   
-
-    function startTimer() {
-    if (intervalId) {
-      clearInterval(intervalId);
+      });
     }
-
-    const timerElem = document.getElementById("timer");
-    const word = this.getWord();
-    const wordSymbols = word.length;
-
-    if (wordSymbols === 0) {
-    console.warn("getWord() вернула пустую строку");
-    timeLimit = 0;
-    timerElem.textContent = "0";
-    return;
-    }
-    timeLimit = wordSymbols;
-    timerElem.textContent = timeLimit;
-
-function decrease() {
-    
-    timeLimit--;
-    timerElem.textContent = timeLimit; //перезаписываем уменьшенное время
-if (timeLimit <= 0) {
-clearInterval(intervalId);
-intervalId = null;
-timerElem.textContent = "0";
-alert("Время вышло!!!(-_-)");
-}
-}
-
-const intervalId = setInterval(decrease, 1000);
-  };
-     /* TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода символа вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
-  }
 
   success() {
-    if(this.currentSymbol.classList.contains("symbol_current")) this.currentSymbol.classList.remove("symbol_current");
+    if(this.currentSymbol.classList.contains("symbol_current")) {
+      this.currentSymbol.classList.remove("symbol_current");
+    }
+
     this.currentSymbol.classList.add('symbol_correct');
     this.currentSymbol = this.currentSymbol.nextElementSibling;
 
@@ -88,7 +53,7 @@ const intervalId = setInterval(decrease, 1000);
       return;
     }
 
-    if (++this.winsElement.textContent === 10) {
+    if (Number(this.winsElement).textContent === 10) {
       alert('Победа!');
       this.reset();
     }
@@ -96,7 +61,7 @@ const intervalId = setInterval(decrease, 1000);
   }
 
   fail() {
-    if (++this.lossElement.textContent === 5) {
+    if (Number(this.lossElement).textContent === 5) {
       alert('Вы проиграли!');
       this.reset();
     }
@@ -105,11 +70,47 @@ const intervalId = setInterval(decrease, 1000);
 
   setNewWord() {
     const word = this.getWord();
-
     this.renderWord(word);
-    startTimer();
-
+    this.startTimer();
   }
+
+  startTimer() {
+
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+
+    const timerElem = document.getElementById("timer");
+    if (!timerElem) return;
+
+    const word = this.getWord();
+    const wordSymbols = word.length;
+
+    if (wordSymbols === 0) {
+    console.warn("getWord() вернула пустую строку");
+    this.timeLimit = 0;
+    timerElem.textContent = "0";
+    return;
+    }
+
+    this.timeLimit = wordSymbols;
+    timerElem.textContent = this.timeLimit;
+
+const decrease = () => {
+  this.timeLimit--;
+  timerElem.textContent = this.timeLimit; //перезаписываем уменьшенное время
+
+    if (this.timeLimit <= 0) {
+clearInterval(this.intervalId);
+this.intervalId = null;
+timerElem.textContent = "0";
+alert("Время вышло!!!(-_-)");
+}
+};
+
+this.intervalId = setInterval(decrease, 1000);
+  };
 
   getWord() {
     const words = [
@@ -137,11 +138,11 @@ const intervalId = setInterval(decrease, 1000);
           `<span class="symbol ${i === 0 ? 'symbol_current': ''}">${s}</span>`
       )
       .join('');
-    this.wordElement.innerHTML = html;
 
+    this.wordElement.innerHTML = html;
     this.currentSymbol = this.wordElement.querySelector('.symbol_current');
   }
 }
 
-new Game(document.getElementById('game'))
+new Game(document.getElementById('game'));
 
