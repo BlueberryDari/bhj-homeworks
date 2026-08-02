6c799ae3ee1a07a9f5753c863ef4dfd250ebec23
BlueberryDari <avlertaver@gmail.com>
2026-08-02 20:08:06 +0300

    tabs edited

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,18 +1,18 @@
 const blocks = document.querySelectorAll(".tabs");
 
 blocks.forEach(block => {
-  const tabsCollection = document.querySelector(".tab");
+  const tabsCollection = document.querySelectorAll(".tab");
   const contentsCollection = document.querySelector(".tab__content");
 
   tabsCollection.forEach((tab, index) => {
 
-      tabsCollection.forEach(t => t.classList.remove(".tab_active"));
-      contentsCollection.forEach(c => c.classList.remove(".tab__content_active"));
+      tabsCollection.forEach(t => t.classList.remove("tab_active"));
+      contentsCollection.forEach(c => c.classList.remove("tab__content_active"));
 
       tab.addEventListener("click", () => {
 
         tab.classList.add("tab_active");
-        contentsCollection[index].add(".tab__content_active");
+        contentsCollection[index].add("tab__content_active");
     });
   });
 });
